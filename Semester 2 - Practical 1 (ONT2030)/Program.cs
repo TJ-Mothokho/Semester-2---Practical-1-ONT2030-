41e2032b525618d2e9b12d89b2d8ddd1759b72da
s225258358 <s225258358@M104-65>
2023-08-07 10:50:42 +0200

    Add project files.

---
+++ Semester 2 - Practical 1 (ONT2030)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Semester_2___Practical_1__ONT2030_
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
